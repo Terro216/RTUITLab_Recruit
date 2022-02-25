708e56bef30237a454eca70cdd39df6b3d1f2177
Terro216 <0ilya@bk.ru>
2022-02-25 23:31:41 +0300

    done header v1.0 (styles,animation,etc)

--- src/components/header.js
+++ src/components/header.js
@@ -28,25 +28,43 @@ function Hamburger() {
 export function Header() {
 	return (
 		<header className='header-wrapper'>
-			<div className='header-logo'>
+			<NavLink to='/' className='header-logo'>
 				<span className='header-logo-top'>Честный бро</span>
 
 				<div className='header-logo-bottom'>
-					<img className='header-logo-icon' src={Heart} alt='red heart logo icon'></img>
+					<img
+						className='header-logo-icon'
+						src={Heart}
+						width='36px'
+						height='36px'
+						alt='red heart logo icon'></img>
 					<span className='header-logo-ker'>кер</span>
 				</div>
-			</div>
+			</NavLink>
 			<nav className='header-links'>
-				<NavLink to='/about' className={({ isActive }) => (isActive ? activeClassName : 'header-link')}>
+				<NavLink
+					to='/about'
+					className={({ isActive }) => (isActive ? 'header-link header-link--active' : 'header-link')}>
 					О&nbsp;нас
 				</NavLink>
-				<NavLink to='/business' className={({ isActive }) => (isActive ? activeClassName : 'header-link')}>
+				<NavLink
+					to='/tariff'
+					className={({ isActive }) => (isActive ? 'header-link header-link--active' : 'header-link')}>
+					Тарифы
+				</NavLink>
+				<NavLink
+					to='/business'
+					className={({ isActive }) => (isActive ? 'header-link header-link--active' : 'header-link')}>
 					Бизнесу
 				</NavLink>
-				<NavLink to='/learning' className={({ isActive }) => (isActive ? activeClassName : 'header-link')}>
+				<NavLink
+					to='/learning'
+					className={({ isActive }) => (isActive ? 'header-link header-link--active' : 'header-link')}>
 					Обучение
 				</NavLink>
-				<button className='header-loginButton'>Стать клиентом!</button>
+				<NavLink to='/login' className='header-loginButton'>
+					Стать клиентом!
+				</NavLink>
 			</nav>
 			<Hamburger />
 		</header>
