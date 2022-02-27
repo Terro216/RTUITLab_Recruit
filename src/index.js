d24277665c6a1ca5ff1752a5f8e61cec630dc8e7
Terro216 <0ilya@bk.ru>
2022-02-28 02:38:26 +0300

    wrap app in strictmode and browserouter + styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { App } from './App.js'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './App.js'
+import './index.scss'
+
+ReactDOM.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
