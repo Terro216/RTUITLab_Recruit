b72ef9d4eefa8291b4168ca896a1aa1129f120ec
Terro216 <0ilya@bk.ru>
2022-03-05 01:23:33 +0300

    separate for reusing

---
+++ src/components/helpers/hamburger.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react'
+import './hamburger.scss'
+
+export function Hamburger(props) {
+	function toggleState() {
+		document.querySelector('.hamburger-icon').classList.toggle('openHamburger')
+		if (props.from == 'header') {
+			document.querySelector('.header-links').classList.toggle('showLinks')
+			//document.querySelector('.header-links').style.display = 'flex'
+			//animateCSS('.sidebar-wrapper', 'slideInDown')
+		} else {
+			document.querySelector('.cabinetAside-wrapper').classList.toggle('asideDisplay')
+		}
+	}
+
+	useEffect(() => {
+		props.toggleHamburger.current = toggleState
+	}, [props.toggleHamburger])
+
+	return (
+		<div className={props.from == 'header' ? 'header-hamburger' : 'aside-hamburger'}>
+			<div className='hamburger-icon' onClick={toggleState}>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	)
+}
