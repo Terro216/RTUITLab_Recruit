6c12a77b8725ebef3e4df84aad1c4c6458ee89fb
Terro216 <0ilya@bk.ru>
2022-03-11 02:58:33 +0300

    add some checking functions

---
+++ src/scripts/functions.js
@@ -0,0 +1,11 @@
+function checkMobileRegex(number) {
+	return /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/gm.test(number)
+}
+
+function checkMail(mail) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gm.test(
+		mail
+	)
+}
+
+export { checkMobileRegex, checkMail }
