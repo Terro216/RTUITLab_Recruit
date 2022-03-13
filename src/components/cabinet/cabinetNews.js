832cf9ccfe31dab48651f7cde460e96b5db8ed41
Terro216 <0ilya@bk.ru>
2022-03-14 01:43:40 +0300

    add news page

--- src/components/cabinet/cabinetNews.js
+++ src/components/cabinet/cabinetNews.js
@@ -1,6 +1,47 @@
 import './styles/cabinetNews.scss'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ReactDom from 'react-dom'
+import { News } from '../helpers/news.js'
 
 export function CabinetNews() {
-	return <section className='profile-news'>news</section>
+	let [newsList, setNewsList] = useState(null)
+
+	async function getNewsList() {
+		const apiToken = 'HermQXrbr1zsGMKKqZmBkDmXz9vXpWLYVTbvYMxx'
+
+		await fetch(`https://api.marketaux.com/v1/news/all?api_token=${apiToken}&language=ru&sentiment_gte=0.5`)
+			.then((res) => res.json())
+			.then((res) => {
+				setNewsList(res.data)
+			})
+			.catch((err) => {
+				console.error(err)
+				alert('Произошла ошибка приполучении новостей. обновите страницу')
+				setNewsList('error')
+			})
+	}
+
+	function addNews() {
+		newsList.forEach((elem) => {
+			let item = document.createElement('div')
+			item.classList.add('news-item')
+			ReactDom.render(<News data={elem} />, item)
+			document.querySelector('.news-wrapper').appendChild(item)
+		})
+	}
+
+	useEffect(() => {
+		document.querySelector('.news-wrapper').innerHTML = ''
+		if (newsList === null) {
+			getNewsList()
+			return
+		}
+		addNews()
+	}, [newsList])
+
+	return (
+		<section className='profile-news'>
+			<h2 className='news-header'>Новости рынка</h2> <div className='news-wrapper'></div>
+		</section>
+	)
 }
---
+++ src/components/helpers/news.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './news.scss'
+export function News({ data }) {
+	return (
+		<article className='news'>
+			<img className='news-image' src={data.image_url} width='100' height='100' />
+			<h3 className='news-head'>{data.title}</h3>
+			<div className='news-content'>
+				{
+					data.snippet //data.description ? data.description :
+				}
+			</div>
+			<a href={data.url} className='news-link' target='_blank' rel='noreferrer'>
+				Читать подробнее
+			</a>
+		</article>
+	)
+}
