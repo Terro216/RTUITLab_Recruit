1dc94b57cc018ccefbefc80d12fe2a30719e249c
Terro216 <0ilya@bk.ru>
2022-03-14 01:47:02 +0300

    correct price (add amount) and remove argument

--- src/scripts/trade.js
+++ src/scripts/trade.js
@@ -2,9 +2,10 @@ import { db } from './firebaseAuth.js'
 import { doc, setDoc, updateDoc, getDoc, deleteField } from 'firebase/firestore'
 import { getTimeEpoch } from './functions.js'
 
-async function trade(auth, user, action, name, amount, price) {
+async function trade(auth, action, name, amount, price) {
 	let [ticker, exchange] = name.split('.')
 	let time = getTimeEpoch()
+	let user = auth.user
 	if (action === 'buy') {
 		const portfolioHistory = doc(db, 'users', user.id, 'portfolio/history/buys', time)
 		const portfolio = doc(db, 'users', user.id, 'portfolio', 'data')
@@ -17,12 +18,12 @@ async function trade(auth, user, action, name, amount, price) {
 		let currentAmount =
 			(await getDoc(portfolio)
 				.then((doc) => doc.data())
-				.then((data) => +data[ticker][exchange]) //data[name + '_amount'])
+				.then((data) => +data[ticker][exchange])
 				.catch(() => 0)) || 0
 		await updateDoc(portfolio, {
 			[name]: currentAmount + amount,
 		})
-		await auth.changeBalance(+user.balance - price) //не обновляет баланс
+		await auth.changeBalance(+user.balance - price * amount)
 	} else if (action === 'sell') {
 		const portfolioHistory = doc(db, 'users', user.id, 'portfolio/history/sells', time)
 		const portfolio = doc(db, 'users', user.id, 'portfolio', 'data')
