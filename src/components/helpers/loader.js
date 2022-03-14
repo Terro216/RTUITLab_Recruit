cbf0ba0831e6652487acea0fcd0323c555eb6fc5
Terro216 <0ilya@bk.ru>
2022-03-14 21:20:26 +0300

    add spinner

---
+++ src/components/helpers/loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loader.scss'
+
+export function Loader() {
+	return (
+		<div className='loader-spinner'>
+			<div className='inner'>
+				<div></div>
+			</div>
+		</div>
+	)
+}
