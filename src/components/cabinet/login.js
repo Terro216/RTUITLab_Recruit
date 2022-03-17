b3302ab82a2e559a6bfd7c0c99e141c311c699bb
Terro216 <0ilya@bk.ru>
2022-03-17 03:25:56 +0300

    add animation, modal function, fixes

--- src/components/cabinet/login.js
+++ src/components/cabinet/login.js
@@ -5,6 +5,7 @@ import { useAuth } from '../../scripts/firebaseAuth.js'
 import { getCookie } from '../../scripts/cookie.js'
 import { checkMobileRegex } from '../../scripts/functions.js'
 import { Modal } from '../helpers/modal.js'
+import { animateCSS } from '../../scripts/functions.js'
 
 export function Login() {
 	let [modalContent, changeModalContent] = useState({
@@ -17,6 +18,24 @@ export function Login() {
 	let location = useLocation()
 	let auth = useAuth()
 	let from = location.state?.from?.pathname || '/'
+
+	function showModal(state) {
+		if (state === 'error') {
+			changeModalContent({
+				head: 'Ошибка!',
+				body: 'Пожалуйста, попробуйте еще раз',
+				show: true,
+				callback: handleModalCallback,
+			})
+		} else if (state === 'success') {
+			changeModalContent({
+				head: 'Регистрация прошла успешно!',
+				body: 'Войдите, что бы получить свои подарочные 1000$',
+				show: true,
+				callback: handleModalCallback,
+			})
+		}
+	}
 
 	function handleModalCallback() {
 		changeModalContent({
@@ -36,6 +55,7 @@ export function Login() {
 		} else if (getCookie('mobile') !== undefined) {
 			document.querySelector('#telInput').value = getCookie('mobile') // вставка номера если он уже вводился
 		}
+		animateCSS('.login-tel-form', 'fadeIn')
 	}, [])
 
 	function handleMobile(event) {
@@ -47,25 +67,20 @@ export function Login() {
 		if (checkMobileRegex(mobile)) {
 			auth.checkMobile(mobile, (hasError, hasNum, name) => {
 				if (hasError) {
-					//alert('Произошла ошибка. Пожалуйста, попробуйте еще раз')
-					changeModalContent({
-						head: 'Ошибка!',
-						body: 'Слишком короткий пароль',
-						show: true,
-						callback: handleModalCallback,
-					})
+					showModal('error')
 					document.querySelector('.login-tel-form').reset()
 					return 0
 				} else if (hasNum === true) {
 					document.querySelector('.login-tel-form').classList.add('hidden')
 					document.querySelector('.login-password-form').classList.remove('hidden')
-					document.querySelector('.login-form-welcome--password').innerText = `Рады видеть тебя, ${name}!`
+					animateCSS('.login-password-form', 'fadeIn')
+					document.querySelector('.login-form-welcome--password').innerText = `Рады видеть вас, ${name}!`
 				} else {
 					document.querySelector('.login-tel-form').classList.add('hidden')
-					document.querySelector('.tel-error-message').classList.add('hidden')
+					animateCSS('.login-register-form', 'fadeIn')
 					document.querySelector('.login-register-form').classList.remove('hidden')
-					//document.querySelector('.registerInput').setAttribute('value', mobile)
 				}
+				document.querySelector('.tel-error-message').classList.add('hidden')
 			})
 		} else {
 			document.querySelector('.tel-error-message').classList.remove('hidden')
@@ -77,16 +92,10 @@ export function Login() {
 
 		let formData = new FormData(event.currentTarget)
 		let pass = formData.get('password')
-		let mobile = getCookie('mobile')
-		auth.signIn(mobile, pass, (hasError, correctPass) => {
+
+		auth.signIn(pass, (hasError, correctPass) => {
 			if (hasError) {
-				//alert('Произошла ошибка. Пожалуйста, попробуйте еще раз')
-				changeModalContent({
-					head: 'Ошибка!',
-					body: 'Пожалуйста, попробуйте еще раз',
-					show: true,
-					callback: handleModalCallback,
-				})
+				showModal('error')
 				document.querySelector('.login-password-form').reset()
 			} else if (!correctPass) {
 				document.querySelector('.login-password-form').reset()
@@ -104,29 +113,18 @@ export function Login() {
 		let formData = new FormData(event.currentTarget)
 		let name = formData.get('name')
 		let pass = formData.get('password')
+		let mobile = getCookie('mobile')
 		if (pass.length < 6) {
 			document.querySelector('.register-error-message').classList.remove('hidden')
-			return 0
+			return
 		}
-		let mobile = getCookie('mobile')
+
 		auth.register(name, pass, mobile, (hasError) => {
 			if (hasError) {
-				//alert('Произошла ошибка. Пожалуйста, попробуйте еще раз')
-				changeModalContent({
-					head: 'Ошибка!',
-					body: 'Пожалуйста, попробуйте еще раз',
-					show: true,
-					callback: handleModalCallback,
-				})
+				showModal('error')
 				document.querySelector('.login-register-form').reset()
 			} else {
-				//alert('Регистрация прошла успешно! Теперь осталось только войти')
-				changeModalContent({
-					head: 'Регистрация прошла успешно!',
-					body: 'Войдите, что бы получить свои подарочные 1000$',
-					show: true,
-					callback: handleModalCallback,
-				})
+				showModal('success')
 				document.querySelector('.login-tel-form').classList.remove('hidden')
 				document.querySelector('.login-register-form').classList.add('hidden')
 				document.querySelector('.register-error-message').classList.add('hidden')
@@ -138,8 +136,8 @@ export function Login() {
 		<div className='login-wrapper'>
 			<Modal props={modalContent} />
 
-			<Link to='/'>
-				<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512' className='back'>
+			<Link to='/' className='back'>
+				<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
 					<path
 						fill='white'
 						d='M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z'
