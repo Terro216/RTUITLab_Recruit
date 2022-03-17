b7ec67ef16d6111cec69de73980fb2ee5646a942
Terro216 <0ilya@bk.ru>
2022-03-17 03:28:05 +0300

    remove unnecessary and config trade

--- src/components/helpers/portfolioItem.js
+++ src/components/helpers/portfolioItem.js
@@ -4,30 +4,32 @@ import { trade } from '../../scripts/trade.js'
 import { getInfo } from '../../scripts/functions.js'
 
 function PortfolioItem(props) {
-	let [elem, deleteElem] = useState(true)
 	let [data, setData] = useState(props.ticker)
+
 	async function updateTicker() {
 		let newData = await getInfo(props.ticker + '.' + props.exchanges[0])
 		setData(newData)
 	}
+
 	useEffect(() => {
 		updateTicker()
 	}, [])
-	return elem ? (
+
+	return (
 		<div className='item'>
 			<div className='item-name'>{data.name}</div>
 			<div className='item-val'>{props.count} шт.</div>
 			<button
 				className='sell-button'
 				onClick={() => {
-					trade(props.auth, 'sell', props.ticker, props.count, +data.true_price * +props.count)
-					deleteElem(false)
-					props.callback()
+					trade(props.auth, 'sell', props.ticker, +props.count, +data.true_price, () => {
+						props.callback()
+					})
 				}}>
 				Продать
 			</button>
 		</div>
-	) : null
+	)
 }
 
 export { PortfolioItem }
